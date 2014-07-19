c0db75059380354ad7d1445cf4382067b9940e6d
Jan <janmanaloto@gmail.com>
2014-07-19 21:39:58 +0800

    designs

--- theme/js/script.js
+++ theme/js/script.js
@@ -14,3 +14,26 @@ $('#register').click(function() {
   console.log("login");
 }
   );
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 100) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".navbar-brand").removeClass("navbar-visibility",1000);
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".navbar-brand").addClass("navbar-visibility",1000);
+    }
+});
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+        $('body').css('overflow-y','auto');
+        setTimeout(aniAbout, 1000);
+    });
+    
+});
